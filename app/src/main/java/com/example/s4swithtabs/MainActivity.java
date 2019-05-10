781e42e3ae49eff756a80f898e9a8165e6d23357
Denis Matafonov <dmatafonov2000@mail.ru>
2019-05-10 08:45:59 +0500

    Add autentification/registration.

--- app/src/main/java/com/example/s4swithtabs/MainActivity.java
+++ app/src/main/java/com/example/s4swithtabs/MainActivity.java
@@ -4,9 +4,12 @@ import android.app.TabActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TabHost;
+import android.widget.Toast;
 
 import com.example.s4swithtabs.EventsActivity;
 import com.example.s4swithtabs.MessengesActivity;
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends TabActivity {
     /** Called when the activity is first created. */
@@ -14,6 +17,31 @@ public class MainActivity extends TabActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseAuth mAuth;
+        mAuth = FirebaseAuth.getInstance();
+
+        if(mAuth.getCurrentUser() == null) {
+            // Start sign in/sign up activity
+            startActivityForResult(
+                    AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .build(),
+                    -1
+            );
+        } else {
+            // User is already signed in. Therefore, display
+            // a welcome Toast
+            Toast.makeText(this,
+                    "Welcome " + mAuth
+                            .getCurrentUser()
+                            .getDisplayName(),
+                    Toast.LENGTH_LONG)
+                    .show();
+
+            // Load chat room contents
+            displayChatMessages();
+        }
 
         // получаем TabHost
         TabHost tabHost = getTabHost();
@@ -37,5 +65,34 @@ public class MainActivity extends TabActivity {
         tabSpec.setIndicator("Настройки");
         tabSpec.setContent(new Intent(this, SettingsActivity.class));
         tabHost.addTab(tabSpec);
+    }
+
+    private void displayChatMessages() {
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == -1) {
+            if(resultCode == RESULT_OK) {
+                Toast.makeText(this,
+                        "Successfully signed in. Welcome!",
+                        Toast.LENGTH_LONG)
+                        .show();
+                displayChatMessages();
+            } else {
+                Toast.makeText(this,
+                        "We couldn't sign you in. Please try again later.",
+                        Toast.LENGTH_LONG)
+                        .show();
+
+                // Close the app
+                finish();
+            }
+        }
+
     }
 }
