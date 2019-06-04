a9cffdb30302f949f39031503161b392fe071817
Gar1NN <gartas.red@yandex.ru>
2019-06-04 10:32:10 +0500

    Логотип забыли

--- app/src/main/java/com/example/s4swithtabs/SplashScreen.java
+++ app/src/main/java/com/example/s4swithtabs/SplashScreen.java
@@ -14,11 +14,12 @@ public class SplashScreen extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent i=new Intent(SplashScreen.this, MainActivity.class);
+                Intent i = new Intent(SplashScreen.this, MainActivity.class);
                 startActivity(i);
                 finish();
             }
-        },2*1000);}
+        }, 2 * 1000);
     }
+}
 
 
