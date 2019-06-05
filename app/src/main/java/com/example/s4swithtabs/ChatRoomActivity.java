bcff5c415791662cbb37966937945ee0eab3e7ef
Gar1NN <gartas.red@yandex.ru>
2019-06-05 16:57:34 +0500

    Исправленна критическая ошибка для устройств с android api 22 и ниже

--- app/src/main/java/com/example/s4swithtabs/ChatRoomActivity.java
+++ app/src/main/java/com/example/s4swithtabs/ChatRoomActivity.java
@@ -4,16 +4,12 @@ import android.annotation.SuppressLint;
 import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Color;
-import android.graphics.ColorSpace;
 import android.net.Uri;
-import android.provider.MediaStore;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.text.format.DateFormat;
 import android.view.View;
 import android.widget.EditText;
@@ -29,28 +25,22 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.collection.LLRBNode;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
-import com.bumptech.glide.Glide;
 
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.URL;
 import java.util.UUID;
 
 
 public class ChatRoomActivity extends AppCompatActivity {
-    public String name;
-    private Uri filePath;
-    FirebaseStorage firebaseStorage = FirebaseStorage.getInstance();
-    StorageReference storageReference = firebaseStorage.getReference();
     private final int PICK_IMAGE_REQUEST = 71;
+    public String name;
     public String pathImg = "images/shedevr.jpg";
     public Uri imgUri;
+    FirebaseStorage firebaseStorage = FirebaseStorage.getInstance();
+    StorageReference storageReference = firebaseStorage.getReference();
+    private Uri filePath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
