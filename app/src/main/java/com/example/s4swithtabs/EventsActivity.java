22f2e4c64ae71244c21bc5581b3a7b2b92bb1cbe
Denis Matafonov <dmatafonov2000@mail.ru>
2019-06-05 19:00:55 +0500

    Мои события теперь - только с текущей даты. Старые только во всех. Чаты старые не удаляем - пропишем так, чтобы человек сам мог это сделать.

--- app/src/main/java/com/example/s4swithtabs/EventsActivity.java
+++ app/src/main/java/com/example/s4swithtabs/EventsActivity.java
@@ -346,7 +346,7 @@ public class EventsActivity extends AppCompatActivity {
         String user = FirebaseAuth.getInstance().getCurrentUser().getDisplayName();
         //Toast.makeText(this,user,Toast.LENGTH_LONG).show();
         adapter = new FirebaseListAdapter<EventModel>(this, EventModel.class,
-                R.layout.event, FirebaseDatabase.getInstance().getReference().child(user)) { //WARNING//
+                R.layout.event, FirebaseDatabase.getInstance().getReference().child(user).orderByChild("eventTime").startAt(Calendar.getInstance().getTimeInMillis())) { //WARNING//
             @Override
             protected void populateView(View v, EventModel model, int position) {
 
